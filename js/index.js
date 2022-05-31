ed8a063ad5dab3bea210ae4869d592225cc440af
Morshaedul <morshaedulanirbanit@gmail.com>
2022-05-31 10:22:12 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+    $("#st1").click(function() {
+        $(".fa-star").css("color", "black");
+        $("#st1").css("color", "yellow");
+
+    });
+    $("#st2").click(function() {
+        $(".fa-star").css("color", "black");
+        $("#st1, #st2").css("color", "yellow");
+
+    });
+    $("#st3").click(function() {
+        $(".fa-star").css("color", "black")
+        $("#st1, #st2, #st3").css("color", "yellow");
+
+    });
+    $("#st4").click(function() {
+        $(".fa-star").css("color", "black");
+        $("#st1, #st2, #st3, #st4").css("color", "yellow");
+
+    });
+    $("#st5").click(function() {
+        $(".fa-star").css("color", "black");
+        $("#st1, #st2, #st3, #st4, #st5").css("color", "yellow");
+
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('h1').classList.add("para1")
+
+// $('h1').css({'color':'red',
+//             'font-style':'bold',
+//             "background-color": "black",
+//             "font-size": "60px"
+
+// });
+
+// button click  function:
+// document.querySelector('button').addEventListener('click', function(e) {
+//     document.querySelector("h1").innerHTML="Button is clicked."
+// })
+
+// same working below this code 
+
+// $('button').click( function() {
+//     $("h1").toggleClass('para1 button');
+// })
+
+// var num1=parseInt(prompt("Enter your second  value"))
+// var num2=parseInt(prompt("Enter your second  value"))
+// $('#btn').click( function() {
+//     function add(num1, num2) {
+//         v=num1+num2;
+//         return v;
+//     }
+//     var n= add(num1, num2);
+//     document.write(n)
+// })
+// $('#btn').click(function(e) {
+//     $('#btn').css('background',"black")
+//     $('#btn').css('color',"red")
+// })
+// $("#click_btn").click(function(e) {
+//     $('#click_btn').css('background',"black")
+//     $('#click_btn').css('color','red')
+// })
+
+// $("#click_btn").click(function(e) {
+//     $('#click_btn').css('background',"black")
+//     $('#click_btn').css('color','red')
+// })
+
+
+
+
